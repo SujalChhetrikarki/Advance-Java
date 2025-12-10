a630fce39bed1b8e3891e0023d6d82e55a18dc70
SujalChhetriKarki <Xetrisujal2003@gmail.com>
2025-12-10 12:00:54 +0545

    hello

---
+++ src/Lab1/one.java
@@ -0,0 +1,28 @@
+package Lab1;
+
+/* File: AwtCloseFrame.java
+Uses AWT Frame and handles window close event to exit the program. dfhjdslfkjdfjn
+*/
+import java.awt.*;
+import java.awt.event.*;
+
+public class one {
+ public static void main(String[] args) {
+     Frame f = new Frame("AWT Close Example");
+     f.setSize(400, 200);
+     f.setLayout(new FlowLayout());
+     Button btn = new Button("Click me");
+     f.add(btn);
+
+     // handle window close (close-box)
+     f.addWindowListener(new WindowAdapter() {
+         @Override
+         public void windowClosing(WindowEvent e) {
+             f.dispose();        // close the Frame
+             System.exit(0);     // ensure JVM exits
+         }
+     });
+
+     f.setVisible(true);
+ }
+}
